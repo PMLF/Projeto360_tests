d1b2cb7abacbcb4d870227c8510b20c920f10b42
PMLF <pmlfonseca@outlook.com>
2020-07-25 18:15:06 +0100

    Added covering element behind appearing QR Code.

--- js/getQRCode.js
+++ js/getQRCode.js
@@ -1,14 +1,17 @@
 document.querySelector(".get-qrcode").addEventListener('click',
     function (e) {
         let qrcode = document.querySelector('.qrcode');
+        let cover = document.querySelector('.cover-screen');
         if (qrcode.classList.contains("qr-up")) {
             qrcode.classList.remove("qr-up");
             qrcode.classList.add("qr-down");
+            cover.classList.add('hidden');
             this.innerHTML = 'Gerar Código QR';
         }
         else {
             qrcode.classList.remove("qr-down");
             qrcode.classList.add("qr-up");
+            cover.classList.remove('hidden');
             this.innerHTML = 'Esconder código QR';
         }
     }
