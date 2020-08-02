dcb4ec829e3916021ef1a96984066a85d4a700b0
PMLF <pmlfonseca@outlook.com>
2020-08-02 01:58:13 +0100

    Pre-campaign microsite's structure complete. Added titles, social media links, embassador list and countdown.

--- js/main.js
+++ js/main.js
@@ -29,11 +29,33 @@ document.addEventListener("mousemove",
 });
 
 $(document).ready(function(){
-    $('.original').mousemove(function(e) {
+    $('.page-container').mousemove(function(e) {
         let x = e.clientX;
         let y = e.clientY;
       
       $('.negative').css('-webkit-mask-position-x', x - 50);
       $('.negative').css('-webkit-mask-position-y', y - 50);
     });
+
+    let endOfCampaign = new Date("Aug 14, 2020 00:00:00").getTime();
+    let x = setInterval(function() {
+        let now = new Date().getTime();
+
+        let distance = endOfCampaign - now;
+
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        let timeMeasures = [days,hours,minutes,seconds];
+        for (let i = 0; i < timeMeasures.length; i++) {
+            timeMeasures[i] = ('0' + timeMeasures[i]).slice(-2);
+        }
+
+        document.querySelector(".days").innerHTML = timeMeasures[0];
+        document.querySelector(".hours").innerHTML = timeMeasures[1];
+        document.querySelector(".minutes").innerHTML = timeMeasures[2];
+        document.querySelector(".seconds").innerHTML = timeMeasures[3];
+    }, 1000);
 });
